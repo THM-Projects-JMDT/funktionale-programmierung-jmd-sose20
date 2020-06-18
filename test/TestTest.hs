afe59e873d64cb0d9d6e04a06368f524fdb86d27
David Martschenko <david.martschenko@web.de>
2020-06-18 21:36:44 +0200

    Add comments

--- test/TestTest.hs
+++ test/TestTest.hs
@@ -3,15 +3,23 @@ module Main where
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
+{-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Tests for the formatter
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-}
+
+-- do not change
 main = defaultMain unitTests
 
-unitTests =
-  testGroup
-    "Unit tests"
-    [testA, testB]
+-- new tests have to be added to this list
+unitTests = testGroup "Unit tests" 
+  [ testA
+  , testB ]
 
-testA =
-  testCase "1 does not equal 2" $ assertEqual [] 1 2
 
-testB =
-  testCase "1+1 equals 2" $ assertEqual [] (1 + 1) 2
+-- test cases ------------------------------------
+
+testA = testCase "1 does not equal 2" $ assertEqual [] 1 2
+
+testB = testCase "1+1 equals 2" $ assertEqual [] (1 + 1) 2
