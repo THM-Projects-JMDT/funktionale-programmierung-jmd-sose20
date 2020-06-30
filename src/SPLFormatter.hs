a58450b068ec5203e57d6a0f9ba9f3b0fb604188
David Martschenko <david.martschenko@web.de>
2020-06-30 11:50:20 +0200

    Add NewLineStyle to config + adjustments

--- src/SPLFormatter.hs
+++ src/SPLFormatter.hs
@@ -1,6 +1,8 @@
 module SPLFormatter where
 
 import SPLAbsyn
+import Text.Parsec
+import SPLParser
 
 {-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -14,19 +16,24 @@ data Config = Config {
     indentType :: IndentationType,
     indentNum :: Int,
     leftCurlNextLine :: Bool,
-    keepAllComments :: Bool
+    keepAllComments :: Bool,
+    newLineType :: NewLineStyle
 } deriving (Eq, Show)
 
-defaultConfig = Config Tab 1 False False
+defaultConfig = Config Space 2 False True Linux
 
 data IndentationType = Space 
                      | Tab 
                      deriving (Eq, Show)
 
+data NewLineStyle = Linux
+                  | Windows
+                  | ClassicMac
+                  deriving (Eq, Show)
+
 type PrettyPrinter a = Config -> IndentationLevel -> a -> String
 
 type IndentationLevel = Int
-
 
 
 -- utility ---------------------------------------------------
@@ -37,25 +44,39 @@ indent it n c = replicate (n * c) $ case it of
                                       Space -> ' '
                                       Tab   -> '\t'
 
-showComment :: Comment -> String
-showComment c = "//" ++ c ++ "\n"
+                            
+-- for testing -----------------------------------------------
+--------------------------------------------------------------
 
+run :: Parser a -> String -> Either ParseError a
+run p = runParser p () ""
+
+testFormat :: String -> Parser a -> PrettyPrinter a -> IO ()
+testFormat s p f = putStrLn $ case run p s of 
+                                Left _  -> error "Parser failed"
+                                Right r -> f defaultConfig 0 r
 
 
 -- pretty printing -------------------------------------------
 --------------------------------------------------------------
 
 printVariable :: PrettyPrinter (Commented Variable) 
-printVariable conf@(Config it n _ _) c (NamedVariable v, css) = indent it n c
-                                                             ++ v 
-                                                             ++ printComments conf c (head css)
+printVariable conf@(Config it n _ _ _) c (NamedVariable v, css) =  v 
+                                                                ++ printComments conf c (head css)
 
 printLineComment :: PrettyPrinter Comment
-printLineComment (Config it n _ _) c cm = indent it n c 
-                                       ++ showComment cm
+printLineComment conf@(Config it n _ _ _) c cm = indent it n c 
+                                              ++ printComment conf c cm
+
+printComment :: PrettyPrinter Comment
+printComment (Config _ _ _ _ nlt) _ c = "//" ++ c ++ case nlt of
+                                                       Linux      -> "\n"
+                                                       Windows    -> "\r\n"
+                                                       ClassicMac -> "\r"
 
 printComments :: PrettyPrinter [Comment]
-printComments _ _ [] = ""
-printComments conf c (cm:cms) = " " 
-                             ++ showComment cm 
-                             ++ concatMap (printLineComment conf c) cms
+printComments _ _ []                     = ""
+printComments (Config _ _ _ False _) _ _ = ""
+printComments conf c (cm:cms)            = " " 
+                                        ++ printComment conf c cm 
+                                        ++ concatMap (printLineComment conf c) cms
