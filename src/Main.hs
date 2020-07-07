5dfd2ae33d2f45aff40ef4d32b8c37efc2695368
Max Stephan <max.stephan@mni.thm.de>
2020-07-07 10:32:46 +0200

    Main: getArgs and run parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import           System.Environment
+import           Formatter
+import           Parser
+import           Text.Parsec.Token
+import           Text.Parsec
+import           Text.Parsec.Language
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  print (removeEscaped $ head args)
+  putStr $ case run2 pProgram (removeEscaped $ head args) of
+    Left  err -> error (show err)
+    Right r   -> fProgram defaultConfig 0 r
+
+removeEscaped :: String -> String
+removeEscaped s = case runParser p () "" ("\"" ++ s ++ "\"") of
+  Left  err -> "Parser failed: " ++ show err
+  Right r   -> r
+
+p = stringLiteral $ makeTokenParser haskellDef
